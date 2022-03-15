4e01669e2822b7e8dc2f4ed9445a2188b5111c58
Evan Crabtree <ecrab21@gmail.com>
2022-03-15 16:44:30 +0000

    Add files via upload

---
+++ Day 1/Part 2/SonarSums.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int x, y, z, count(0), sum(INT_MAX);
+	string s;
+	ifstream file;
+
+	file.open("input.txt");
+
+	if (file.is_open()) {
+		getline(file, s);
+		x = stoi(s);
+		getline(file, s);
+		y = stoi(s);
+
+		while (getline(file, s)) {
+			z = stoi(s);
+
+			cout << x << " + " << y << " + " << z << " > " << sum << "? : ";
+
+			if (x + y + z > sum) {
+				count++;
+				cout << "true" << endl;
+			}
+			else {
+				cout << "false" << endl;
+			}
+			sum = x + y + z;
+			x = y;
+			y = z;
+		}
+	}
+
+	cout << count;
+
+	cin.get();
+	file.close();
+
+	return 0;
+}
